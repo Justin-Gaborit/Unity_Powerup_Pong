3a5fc793d546e38e92e6fc9ddb5363e65e7871e3
JaysonCaissie <113391142+JaysonCaissie@users.noreply.github.com>
2022-09-30 10:24:24 -0400

    UI stuff

--- Assets/1 - Scripts/mainMenu.cs
+++ Assets/1 - Scripts/mainMenu.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+    /*
     [SerializeField]
     private AudioClip _Select;
 
@@ -14,42 +15,52 @@ public class mainMenu : MonoBehaviour
     private AudioClip _Back;
 
     private AudioSource _audioSource;
+    */
 
     void Start()
     {
-        _audioSource = GetComponent<AudioSource>();
+    //    _audioSource = GetComponent<AudioSource>();
 
-        if (_audioSource == null)
+    /*    if (_audioSource == null)
         {
             Debug.LogError("Audio Source is NULL");
         }
+    */
     }
     
     public void PlayGame()
     {
+        /*
         _audioSource.clip = _Select;
         _audioSource.Play();
+        */
         SceneManager.LoadScene("pong_game_scene");
     }
 
     public void Tutorial()
     {
+        /*
         _audioSource.clip = _Select;
         _audioSource.Play();
+        */
         SceneManager.LoadScene("tutorial_scene");
     }
 
     public void Credits()
     {
+        /*
         _audioSource.clip = _Select;
         _audioSource.Play();
+        */
         SceneManager.LoadScene("credits_scene");
     }
 
     public void Back()
     {
+        /*
         _audioSource.clip = _Back;
         _audioSource.Play();
-        SceneManager.LoadScene("SampleScene");
+        */
+        SceneManager.LoadScene("MainMenuScene");
     }
 }
